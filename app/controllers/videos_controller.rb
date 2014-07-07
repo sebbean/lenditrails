1f42a80be286158784c79b715d54aa667754e3c6
Katherine Santiago <santiago.kat@gmail.com>
2014-07-07 10:37:42 -0400

    added 302 redirect for the /videos-2 page to lendit.tv

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,7 @@
 class VideosController < ApplicationController
+
+  def index
+    redirect_to speakers_path, :status => 301
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ LenditRails::Application.routes.draw do
   get '/about' => 'welcome#about'
   get '/sponsors' => 'sponsors#index'
   get '/speakers' => 'speakers#index'
+  get '/speakers/sign-up' => 'speakers#sign-up'
   get '/thank-you' => 'welcome#thank-you'
   get '/register' => 'welcome#register'
   root 'conferences#europe'
@@ -17,7 +18,9 @@ LenditRails::Application.routes.draw do
   get '/lendit-spring-2014' => 'welcome#go_to_root'
   get '/lendit2013' => 'welcome#go_to_root'
   get '/lendit-2014-san-francisco-presentations' => 'welcome#go_to_root'
-  get '/videos-2' => 'videos#index'
+
+  # 302 redirect to be updated to 301 once Wistia is configured
+  get '/videos-2', to: redirect("http://lendit.tv", status: 302)
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

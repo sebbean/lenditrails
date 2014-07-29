889f893b87021ce9c808a94c9933577761db60b0
Katherine Santiago <santiago.kat@gmail.com>
2014-07-29 13:22:37 -0400

    added sponsors in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Sponsor.delete_all
+
+Sponsor.create(company_name: 'Afluenta', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/afluenta.png')
+Sponsor.create(company_name: 'Blue Elephant', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/blue-elephant.png')
+Sponsor.create(company_name: 'Capital Source', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/capital-source.png')
+Sponsor.create(company_name: 'First Associates', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/first-associates.png')
+Sponsor.create(company_name: 'Funding Circle', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/funding-circle.png')
+Sponsor.create(company_name: 'Kabbage', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/kabbage.png')
+Sponsor.create(company_name: 'Lending Club', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/lending-club.png')
+Sponsor.create(company_name: 'LendKey', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/lendkey.png')
+Sponsor.create(company_name: 'OnDeck', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/ondeck.png')
+Sponsor.create(company_name: 'Opus', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/opus.png')
+Sponsor.create(company_name: 'Orchard', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/orchard.png')
+Sponsor.create(company_name: 'Prosper', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/prosper.png')
+Sponsor.create(company_name: 'Raiseworks', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/raiseworks.png')
+Sponsor.create(company_name: 'RK&O', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/rko.png')
+Sponsor.create(company_name: 'Strategic Funding', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/sfs-capital.png')
+
+Speaker.delete_all

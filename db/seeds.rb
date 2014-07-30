0c0a53bd213c802f9106d04678509001d3ed60b0
Katherine Santiago <santiago.kat@gmail.com>
2014-07-30 11:18:49 -0400

    added 5 sponsors

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,24 @@ Sponsor.delete_all
 Speaker.delete_all
 
 Sponsor.create(company_name: 'Afluenta', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/afluenta.png')
+Sponsor.create(company_name: 'BDO', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/bdo.png')
 Sponsor.create(company_name: 'Blue Elephant', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/blue-elephant.png')
 Sponsor.create(company_name: 'Capital Source', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/capital-source.png')
+Sponsor.create(company_name: 'Cross River Bank', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/cross-river-bank.png')
 Sponsor.create(company_name: 'First Associates', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/first-associates.png')
 Sponsor.create(company_name: 'Funding Circle', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/funding-circle.png')
 Sponsor.create(company_name: 'Kabbage', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/kabbage.png')
 Sponsor.create(company_name: 'Lending Club', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/lending-club.png')
 Sponsor.create(company_name: 'LendKey', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/lendkey.png')
+Sponsor.create(company_name: 'Millennium Trust Company', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/millennium.png')
 Sponsor.create(company_name: 'OnDeck', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/ondeck.png')
 Sponsor.create(company_name: 'Opus', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/opus.png')
 Sponsor.create(company_name: 'Orchard', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/orchard.png')
 Sponsor.create(company_name: 'Prosper', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/prosper.png')
+Sponsor.create(company_name: 'Rainfin', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/rainfin.png')
 Sponsor.create(company_name: 'Raiseworks', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/raiseworks.png')
 Sponsor.create(company_name: 'RK&O', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/rko.png')
+Sponsor.create(company_name: 'Sofi', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/sofi.png')
 Sponsor.create(company_name: 'Strategic Funding', logo_img: 'https://s3-us-west-2.amazonaws.com/lendit/sponsors/sfs-capital.png')
 
 

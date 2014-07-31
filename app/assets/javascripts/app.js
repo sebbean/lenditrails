adc48eab929526f64ed13ee09a4016458a489221
Katherine Santiago <santiago.kat@gmail.com>
2014-07-31 15:30:33 -0400

    shorten europe speakers bio with jquery plugin - v1

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,25 @@ var app = app || {};
 
 $(document).ready(function() {
 
+  // $('.past-speaker-bio').shorten({
+  //   moreText: 'Read More',
+  //   lessText: 'Read Less',
+  //   showChars: '210',
+  // });
+
+  // dynamically shorten and show more link
+  $(".show-more").click(function() {
+        if($(".subhero").hasClass("show-more-height")) {
+            $(this).text("(Show Less)");
+        } else {
+            $(this).text("(Show More)");
+        }
+
+        $(".past-speaker-bio").toggleClass("show-more-height");
+    });
+
+
+  // colorbox for the past speakers
   $('.past-speaker').colorbox({inline:true, innerWidth:"780px", innerHeight:"430px", rel:'past-speaker'});
 
 });
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require colorbox
+//= require shorten
 //= require turbolinks
 //= require_tree .

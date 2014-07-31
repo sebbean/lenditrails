c64fd6739afb06453000f857553e679c6c9158e6
Katherine Santiago <santiago.kat@gmail.com>
2014-07-31 18:19:47 -0400

    added colorbox to current speakers

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,16 +2,17 @@ var app = app || {};
 
 $(document).ready(function() {
 
-  $(".show-more").click(function() {
-      console.log('yayyyyy');
-        if($(".current-speaker-bio").hasClass("show-more-height")) {
-            $(this).text("Show Less");
-        } else {
-            $(this).text("Show More");
-        }
+  // toggleClass
+  // $(".show-more").click(function() {
+  //     console.log('yayyyyy');
+  //       if($(".current-speaker-bio").hasClass("show-more-height")) {
+  //           $(this).text("Show Less");
+  //       } else {
+  //           $(this).text("Show More");
+  //       }
 
-        $(".current-speaker-bio").toggleClass("show-more-height");
-    });
+  //       $(".current-speaker-bio").toggleClass("show-more-height");
+  //   });
 
   // $('.current-speaker-bio').readmore({
   //   maxHeight: 120,
@@ -26,6 +27,10 @@ $(document).ready(function() {
   //   lessText: 'Read Less',
   //   showChars: '210',
   // });
+
+
+  // colorbox for current speakers
+  $('.current-speaker').colorbox({inline:true, rel:'current-speaker', innerWidth:"700px"});
 
   // colorbox for the past speakers
   $('.past-speaker').colorbox({inline:true, innerWidth:"780px", innerHeight:"430px", rel:'past-speaker'});

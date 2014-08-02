a7bdf3acc8d260acc138472c1d951e8682745254
Katherine Santiago <santiago.kat@gmail.com>
2014-08-01 21:40:19 -0400

    added names to different verlag

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(:default, Rails.env)
 module LenditRails
   class Application < Rails::Application
 
-    config.assets.paths << Rails.root.join("assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

1fdda9806edd8733b3e2d98bb308e0cb58545bb6
Katherine Santiago <santiago.kat@gmail.com>
2014-08-18 10:18:15 -0400

    added scrollup bar in vendors and moved bootstrap in vendors assets

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,9 @@ var app = app || {};
 
 $(document).ready(function() {
 
+  // scrollup bar for the nav
+  $('.topbar').scrollupbar();
+
   // speaker form validation
   $("#commentForm").validate({meta: "validate"});
 

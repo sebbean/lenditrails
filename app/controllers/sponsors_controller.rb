0f687fc5479fb5e686d53bc2b434721c20f064a5
Katherine Santiago <santiago.kat@gmail.com>
2014-09-30 12:54:04 -0400

    added europe sponsors

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -10,6 +10,8 @@ class SponsorsController < ApplicationController
     @silver = @europe_sponsors.where(sponsor_level: 'Silver')
     @bronze = @europe_sponsors.where(sponsor_level: 'Bronze')
     @cocktails = @europe_sponsors.where(sponsor_level: 'Pre-Conference Cocktails')
+    @smartphone = @europe_sponsors.where(sponsor_level: 'Smartphone App')
+    @exhibitor = @europe_sponsors.where(sponsor_level: 'Exhibitor')
     @other_sponsors = @europe_sponsors.where(sponsor_level: 'Other')
     @past_sponsors = Sponsor.where(conference: 'LendIt 2014')
   end

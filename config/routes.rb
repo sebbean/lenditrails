3821e55f60b287756092c626ac047d8a4a6abac4
Katherine Santiago <santiago.kat@gmail.com>
2014-10-02 12:54:44 -0400

    added back the routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ LenditRails::Application.routes.draw do
   get '/sharecast' => 'conferences#sharecast'
 
   # get '/home' => 'welcome#home'
-  # get '/europe' => 'conferences#europe'
-  # get '/nyc' => 'conferences#nyc'
-  # get '/china' => 'conferences#china'
+  get '/europe' => 'conferences#europe'
+  get '/nyc' => 'conferences#nyc'
+  get '/china' => 'conferences#china'
   # get '/europe/sponsors' => 'sponsors#europe_sponsors'
   # get '/test' => 'sponsors#test'
 

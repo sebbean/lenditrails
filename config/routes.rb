94d40e1c7121c0ad182b2aee497f5f4d63c2a071
Katherine Santiago <santiago.kat@gmail.com>
2014-10-07 11:21:42 -0400

    updated with company attendance

--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,14 @@ LenditRails::Application.routes.draw do
   # 301 redirects with GA tracking codes
   get '/sharecast' => 'conferences#sharecast'
 
-   # get '/home' => 'welcome#home'
-   # get '/europe' => 'conferences#europe'
-   # get '/nyc' => 'conferences#nyc'
-   # get '/china' => 'conferences#china'
-   # get '/privacy' => 'welcome#privacy'
-   # get '/europe/sponsors' => 'sponsors#europe_sponsors'
-   # get '/test' => 'sponsors#test'
+  get '/home' => 'welcome#home'
+  get '/europe' => 'conferences#europe'
+  get '/nyc' => 'conferences#nyc'
+  get '/china' => 'conferences#china'
+  get '/privacy' => 'welcome#privacy'
+  get '/europe/attendees' => 'welcome#attendees'
+  # get '/europe/sponsors' => 'sponsors#europe_sponsors'
+  # get '/test' => 'sponsors#test'
 
   # See all redirects in WelcomeController
   # 301 redirects from old WP site

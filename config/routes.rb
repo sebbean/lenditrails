5a62fcba62b76e9db44a8764383d4f9798da229d
Katherine Santiago <santiago.kat@gmail.com>
2014-10-30 16:23:33 -0400

    updated home and sponsors pages

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,10 @@ LenditRails::Application.routes.draw do
 
   # get '/europe/sponsors' => 'sponsors#europe_sponsors'
   # get '/europe-test' => 'conferences#europe_test'
-  # get '/home' => 'welcome#home'
-  # get '/nyc' => 'conferences#nyc'
+  get '/home' => 'welcome#home'
+  get '/nyc' => 'conferences#nyc'
   # get '/china' => 'conferences#china'
-  # get '/home-test' => 'welcome#home_test'
+  get '/home-test' => 'welcome#home_test'
 
   # get '/test' => 'sponsors#test'
   # get '/europe/speakers' => 'speakers#europe'

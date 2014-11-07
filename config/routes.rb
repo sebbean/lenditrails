72e509521c0e0df48ff6b943c955a2c1dd049e17
Katherine Santiago <santiago.kat@gmail.com>
2014-11-07 10:08:46 -0500

    updated sponsors and routes for prod

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,10 @@ LenditRails::Application.routes.draw do
   get '/sharecast' => 'conferences#sharecast'
   get '/europe' => 'conferences#europe'
 
-  get '/home' => 'welcome#home'
-  get '/nyc' => 'conferences#nyc'
-  get '/san-francisco-2014/sponsors' => 'sponsors#lendit2014'
-  get '/test' => 'sponsors#test'
+  # get '/home' => 'welcome#home'
+  # get '/nyc' => 'conferences#nyc'
+  # get '/san-francisco-2014/sponsors' => 'sponsors#lendit2014'
+  # get '/test' => 'sponsors#test'
 
   # get '/europe/sponsors' => 'sponsors#europe_sponsors'
   # get '/europe-test' => 'conferences#europe_test'

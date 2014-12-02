1e63359891acf50750079988c06e34bdf112f33d
Katherine Santiago <santiago.kat@gmail.com>
2014-12-02 14:10:13 -0500

    refactored videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,76 +1,17 @@
 class VideosController < ApplicationController
-  before_action :set_video, only: [:show, :edit, :update, :destroy]
 
-  # GET /videos
-  # GET /videos.json
+  def live
+  end
+
   def index
     # @videos = Video.all
     @europe = Conference.where(title: 'LendIt Europe 2014').first
     @euvideos = @europe.videos
   end
 
-  # GET /videos/1
-  # GET /videos/1.json
   def show
+    @video = Video.friendly.find(params[:id])
   end
 
-  # GET /videos/new
-  def new
-    @video = Video.new
-  end
 
-  # GET /videos/1/edit
-  def edit
-  end
-
-  # POST /videos
-  # POST /videos.json
-  def create
-    @video = Video.new(video_params)
-
-    respond_to do |format|
-      if @video.save
-        format.html { redirect_to @video, notice: 'Video was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @video }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
-  def update
-    respond_to do |format|
-      if @video.update(video_params)
-        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /videos/1
-  # DELETE /videos/1.json
-  def destroy
-    @video.destroy
-    respond_to do |format|
-      format.html { redirect_to videos_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_video
-      @video = Video.friendly.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def video_params
-      params.require(:video).permit(:title, :description, :slug, :conference_id, :embed, :img)
-    end
 end

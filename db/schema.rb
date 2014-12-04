d05c159cee6cbd1972aa163031287a0f55cf2816
Katherine Santiago <santiago.kat@gmail.com>
2014-12-04 17:08:03 -0500

    created new seeds and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202160906) do
+ActiveRecord::Schema.define(version: 20141204212615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20141202160906) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
+  create_table "photos", force: true do |t|
+    t.string  "title"
+    t.text    "caption"
+    t.integer "conference_id"
+  end
 
   create_table "speakers", force: true do |t|
     t.string   "first_name"

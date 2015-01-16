6b0b56273d9417994d9facd217218c70e8e4bc36
Katherine Santiago <santiago.kat@gmail.com>
2015-01-16 16:56:04 -0500

    updated usa speakers page to show usa 2014 keynote speakers

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -9,6 +9,7 @@ class SpeakersController < ApplicationController
   def usa15
     @usa15_speakers = Speaker.where(conference: 'LendIt USA 2015')
     @usa15_speakers.sort_by! { |speaker| speaker['last_name'] }
+    @past_speakers = Speaker.where(conference: 'LendIt 2014')
   end
 
   # GET /speakers

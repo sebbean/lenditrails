ea0b8b981896991eab1cbe52e69861bd4f041006
Katherine Santiago <santiago.kat@gmail.com>
2015-01-16 16:56:51 -0500

    redirect /speakers to /usa/2015/speakers

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -3,7 +3,7 @@ class SpeakersController < ApplicationController
 
 
   def index
-    redirect_to ('/europe/2014/speakers'), :status => 301
+    redirect_to ('/usa/2015/speakers'), :status => 301
   end
 
   def usa15

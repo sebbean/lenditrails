e215b406f6e6318ea1178b9678ee63b778127070
Katherine Santiago <santiago.kat@gmail.com>
2015-02-05 17:45:40 -0500

    renamed podcasts view and controller for conference consistency

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,8 +1,12 @@
 class PodcastsController < ApplicationController
 
-  def index
+  def eu14
     @eu14 = Conference.find_by(title: "LendIt Europe 2014")
     @eu14podcasts = Podcast.where(conference_id: @eu14.id)
+  end
+
+  def go_to_eu14
+    redirect_to ('/europe/2014/podcasts'), :status => 301
   end
 
 end

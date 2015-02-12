b2a35142927e8daa8349f0f6aa3b000abc90773c
Katherine Santiago <santiago.kat@gmail.com>
2015-02-12 13:42:08 -0500

    added usa15 agenda with style and magellan

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,18 @@
 var app = app || {};
 
+$(document).foundation('magellan', 'reflow');
+
+$(document).foundation({
+"magellan-expedition": {
+  active_class: 'ag-active', // specify the class used for active sections
+  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+  destination_threshold: 20, // pixels from the top of destination for it to be considered active
+  throttle_delay: 50, // calculation throttling to increase framerate
+  fixed_top: 0, // top distance in pixels assigend to the fixed element on scroll
+  offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+}
+});
+
 $(document).foundation('reveal', {
   animation: 'fadeAndPop',
   animation_speed: 250,
@@ -21,12 +34,11 @@ $(document).foundation('reveal', {
       'display': 'none'
     }
   }
-});
+}
+
+);
+
 $(document).ready(function() {
-
-
-
-
 
 
   $('.menu-item-has-children').on("click", function(){

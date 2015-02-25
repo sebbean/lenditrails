9462f11540c0ad13f02caae9dee58b45df7bdef3
Katherine Santiago <santiago.kat@gmail.com>
2015-02-25 12:25:11 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213202454) do
+ActiveRecord::Schema.define(version: 20150224232559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20150213202454) do
     t.datetime "updated_at"
     t.string   "conference"
     t.integer  "conference_id"
+    t.boolean  "featured"
   end
 
   create_table "sponsors", force: true do |t|

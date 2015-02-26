a3afc4b69e8ca01f0ee1b3155a284a4ad474002e
Katherine Santiago <santiago.kat@gmail.com>
2015-02-26 14:11:10 -0500

    removed agenda route and no index

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ LenditRails::Application.routes.draw do
   get '/audio' => 'welcome#go_to_libsyn'
   get '/bookhotel', to: redirect('https://aws.passkey.com/g/35613120', status: 301)
   get '/podcasts' => 'podcasts#go_to_eu14'
-  get '/agenda' => 'agendas#usa15'
+  # get '/agenda' => 'agendas#usa15'
   # get '/test' => 'sponsors#test'
 
 

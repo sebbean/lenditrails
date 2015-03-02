32b5725c4fdb9256980d834b0a46780fbb5602ae
Katherine Santiago <santiago.kat@gmail.com>
2015-03-02 13:49:30 -0500

    cleaned up conferences controller and views

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -4,26 +4,11 @@ class ConferencesController < ApplicationController
     redirect_to ('/?utm_source=sharecast&utm_medium=video&utm_campaign=europe-2014'), :status => 301
   end
 
-  def china
-  end
-
-  def europe
-  end
-
   def usa15
-  end
-
-  def usa15_startup
-  end
-
-  def usa15_chinese_invite
   end
 
   def go_to_usa15
     redirect_to ('/usa'), :status => 301
-  end
-
-  def europe_test
   end
 
   def usa14

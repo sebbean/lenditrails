e5cc13765283bef136ea5080c604b500de1f8a05
Katherine Santiago <santiago.kat@gmail.com>
2015-04-08 13:58:26 -0400

    updated live to reroute to videos page

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ LenditRails::Application.routes.draw do
   get '/videos' => 'videos#index'
   get '/photos', to: redirect("https://www.flickr.com/photos/wemakepictures/sets/72157647005531163/")
   get '/privacy' => 'welcome#privacy'
-  get '/live' => 'videos#live'
-  #get '/live', to: redirect('/videos', status: 301)
+  # get '/live' => 'videos#live'
+  get '/live', to: redirect('/videos', status: 301)
   # get '/china-pavilion-live' => 'videos#china_live'
   get '/attendees' => 'attendees#go_to_usa15'
   get '/volunteers' => 'welcome#volunteers'

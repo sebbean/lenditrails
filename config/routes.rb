52393a10052db8c24bad67cf2b1203f948ea69fd
Katherine Santiago <santiago.kat@gmail.com>
2015-04-09 18:38:46 -0400

    updated routes for stage

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ LenditRails::Application.routes.draw do
   get '/videos' => 'videos#index'
   get '/photos', to: redirect("https://www.flickr.com/photos/wemakepictures/sets/72157647005531163/")
   get '/privacy' => 'welcome#privacy'
-  # get '/live' => 'videos#live'
-  # get '/china-pavilion-live' => 'videos#china_live'
-  get '/live', to: redirect('/videos', status: 301)
+  get '/live' => 'videos#live'
+  get '/china-pavilion-live' => 'videos#china_live'
+  # get '/live', to: redirect('/videos', status: 301)
   get '/attendees' => 'attendees#go_to_usa15'
   get '/volunteers' => 'welcome#volunteers'
   get '/volunteers/submitted' => 'welcome#volunteers_submitted'
@@ -30,10 +30,10 @@ LenditRails::Application.routes.draw do
 
 
   # LendIt Europe 2015
-  # get '/europe/2015' => 'conferences#eu15'
+  get '/europe/2015' => 'conferences#eu15'
 
   # LendIt China 2015
-  # get '/china' => 'conferences#china15'
+  get '/china' => 'conferences#china15'
 
   # LendIt USA 2015
   get '/usa' => 'conferences#usa15'

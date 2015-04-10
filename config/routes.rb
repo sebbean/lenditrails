6861bdf7b12964292ead39d400e23a20b9f5abb2
Katherine Santiago <santiago.kat@gmail.com>
2015-04-10 14:41:53 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,10 +30,10 @@ LenditRails::Application.routes.draw do
 
 
   # LendIt Europe 2015
-   # get '/europe' => 'conferences#eu15'
+   get '/europe' => 'conferences#eu15'
 
   # LendIt China 2015
-  # get '/china' => 'conferences#china15'
+  get '/china' => 'conferences#china15'
 
   # LendIt USA 2015
   get '/usa' => 'conferences#usa15'

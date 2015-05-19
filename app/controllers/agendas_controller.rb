3e992542a0b13bf9bb173f7dfd7d64b0cc16b55d
Katherine Santiago <santiago.kat@gmail.com>
2015-05-19 14:04:19 -0400

    created agenda routes for china15

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,2 +1,6 @@
 class AgendasController < ApplicationController
+
+  def china15
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,10 @@ LenditRails::Application.routes.draw do
   # LendIt China 2015
   get '/china' => 'conferences#china15'
   get '/china/2015/speakers/sign-up' => 'speakers#china15_sign-up'
+  get '/china/2015/agenda' => 'agendas#china15'
+  get '/china/2015/china-app-show' => 'conferences#china15_china_app'
+  get '/china/2015/bund-summit' => 'conferences#china15_bund_summit'
+
 
   # LendIt USA 2016
   get '/usa/2016' => 'conferences#usa16'

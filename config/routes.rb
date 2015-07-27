84e6e74e42ec4dd0c9fa9b1ba006c6940ecb00f1
Katherine Santiago <santiago.kat@gmail.com>
2015-07-27 14:00:46 -0400

    test sched.org

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ LenditRails::Application.routes.draw do
   # root 'conferences#china15'
 
   get '/home' => 'home#landing'
-  # get '/test' => 'podcasts#index'
+  get '/test' => 'agendas#eu15'
 
   # get '/conferences' => 'welcome#home'
   get '/about' => 'welcome#about'
